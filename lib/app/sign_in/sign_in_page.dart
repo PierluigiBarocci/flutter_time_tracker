cd40b27f8751bbbb93297e9761445309b8330448
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2021-07-17 12:02:41 +0200

    feat: :art: add ElevatedButton widget

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -21,32 +21,27 @@ class SignInPage extends StatelessWidget {
       padding: EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.orange,
-            child: SizedBox(
-              height: 100.0,
+          Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
             ),
           ),
           SizedBox(
             height: 8.0,
           ),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              height: 100.0,
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Sign in with Google'),
+            style: TextButton.styleFrom(
+              primary: Colors.black,
+              backgroundColor: Colors.white,
             ),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              height: 100.0,
-            ),
-          ),
+          )
         ],
       ),
     );
